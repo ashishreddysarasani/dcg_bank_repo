6b835a4788efbaf5ce0154f438ec23d8169431fd
ashishreddysarasani <ashishreddy56789@gmail.com>
2022-11-14 13:43:31 +0530

    updated toit

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
-class test {
+public static class test {
 Demo d = new Demo();
   // printing status
   system.out.printl("listen the demos ");
    system.out.printl("welcome to the demos ");
+void teachble() {}
 }
+
